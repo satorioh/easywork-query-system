8742fb1af7e00d6e3bb6cde4e7987c43b9595213
satorioh <wangbinxp@gmail.com>
2018-04-26 17:18:11 +0800

    integrated angular with AdminLTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
